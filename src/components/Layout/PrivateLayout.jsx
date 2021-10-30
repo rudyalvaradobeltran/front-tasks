e26b7674680e721bf3f0490b637075d24e8e887b
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-10-30 14:12:21 -0300

    feat(navbar): navBar
    NavBar basic component

--- src/components/Layout/PrivateLayout.jsx
+++ src/components/Layout/PrivateLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import NavBar from '../Shared/NavBar';
 
 const PrivateLayout = () => {
   return (
     <React.Fragment>
       <CssBaseline />
+      <NavBar />
       <Container maxWidth="sm">
         Container
       </Container>
---
+++ src/components/Shared/NavBar/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          Photos
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar;

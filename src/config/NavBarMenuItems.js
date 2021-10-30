aeb8a526e481b1a0fe2a9d3d9bc2228feb340e3d
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-10-30 17:55:44 -0300

    feat(navbarmenuitems config): remove edit from navbar menu items
    Remove edit from navbar menu items

--- src/config/NavBarMenuItems.js
+++ src/config/NavBarMenuItems.js
@@ -1,11 +1,9 @@
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
 
 export const NavBarMenuItems = [
   {'name': 'List tasks', 'href': 'list-tasks', 'icon': <FormatListBulletedIcon />},
   {'name': 'Add task', 'href': 'add-task', 'icon': <AddIcon />},
-  {'name': 'Remove task', 'href': 'remove-task', 'icon': <DeleteIcon />},
-  {'name': 'Edit task', 'href': 'edit-task', 'icon': <ModeEditIcon />}
+  {'name': 'Remove task', 'href': 'remove-task', 'icon': <DeleteIcon />}
 ];
